1c4d62ff8b1264e62c4907e1fb75131f6480b14b
Tobias Buschor <tobias.buschor@shwups.ch>
2021-05-26 22:30:03 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,15 +4,30 @@ const min = 'min';
 'href parallax'.split(' ').forEach(attr=>{
     const selector = '[u1-'+attr+']';
     onElement(selector, function(el){
-        import(rootUrl+'attr-'+attr+'/'+attr'.'+min+'.js');
+        import(rootUrl + attr + '.attr/' + attr + '.' + min + '.js');
     });
 });
 
+
 /*
+
+'badge'.split(' ').forEach(attr=>{
+    const selector = '.'+class;
+    onElement(selector, function(el){
+        import(rootUrl + attr + '.class/' + attr + '.' + min + '.css');
+    });
+});
+
 'alert'.split(' ').forEach(element=>{
     const selector = 'u1-'+element;
     onElement(selector, function(el){
-        import(rootUrl+'el-'+element+'/'+element'.'+min+'.js');
+        import(rootUrl+element+'.el/'+element'.'+min+'.js');
     });
 });
+
+function importCss(url){
+    urls[url]
+    document.createElement('link')
+}
+
 */

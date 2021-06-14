73fb43c32b2058746ac9c6339496394c8061657b
Tobias Buschor <tobias.buschor@shwups.ch>
2021-06-14 13:36:03 +0200

    Update auto.js

--- auto.js
+++ auto.js
@@ -5,10 +5,12 @@ import {onElement} from '../js/onElement/mod.js';
 //import 'https://cdn.jsdelivr.net/gh/nuxodin/lazyfill/htmlfills.js';
 
 
-//const rootUrl = 'https://cdn.jsdelivr.net/gh/u1ui/';
-const rootUrl = '../';
-//const min = '.min';
-const min = '';
+const rootUrl = 'https://cdn.jsdelivr.net/gh/u1ui/';
+const min = '.min';
+
+// local testing:
+//const rootUrl =  import.meta.url + '/../../';
+//const min = '';
 
 // attr
 'href parallax ico'.split(' ').forEach(attr=>{

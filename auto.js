0cd838f9a3b3022c6935c7f2d5484d009f659a05
Tobias Buschor <tobias.buschor@shwups.ch>
2021-07-19 20:15:30 +0200

    Update auto.js

--- auto.js
+++ auto.js
@@ -8,7 +8,6 @@ let min = '.min';
 //import 'https://cdn.jsdelivr.net/gh/nuxodin/lazyfill/htmlfills.js';
 
 import {importCss} from './utils.js';
-import 'https://cdn.jsdelivr.net/gh/u1ui/js@1.8.0/init.js';
 import {onElement} from 'https://cdn.jsdelivr.net/gh/u1ui/js@1.8.0/onElement/mod.js';
 
 
@@ -21,6 +20,11 @@ const needed = { js:{}, css:{} };
 function impJs(url){
     if (url in needed.js) return;
     needed.js[url] = prio;
+    var scriptEl = document.createElement('script');
+    scriptEl.type='module';
+    scriptEl.src = url;
+    document.head.append(scriptEl);
+    return;
     return import(url)
 }
 function impCss(url, options={}){
@@ -44,9 +48,9 @@ impCss(rootUrl+'classless.css/variables'+min+'.css');
 impCss(rootUrl+'classless.css/classless'+min+'.css');
 impCss(rootUrl+'classless.css/more'+min+'.css');
 impCss(rootUrl+'classless.css/aria'+min+'.css');
-impCss(rootUrl+'classless.css/simple'+min+'.css');
+//impCss(rootUrl+'classless.css/simple'+min+'.css');
 
-
+impJs(rootUrl+'js/init'+min+'.js');
 
 
 // attr
@@ -77,12 +81,57 @@ impCss(rootUrl+'classless.css/simple'+min+'.css');
 });
 
 
-window.u1Hints = function(){
-    let strCss = Object.entries(needed.css).filter(([,prio])=>prio===1).map(([url,prio])=>'<link rel="stylesheet" href="'+url+'" crossorigin>').join('\n');
-    let strJs  = Object.entries(needed.js).map(([url,prio])=>'<script src="'+url+'" type=module crossorigin></script>').join('\n');
-    let strCssNonCritical = Object.entries(needed.css).filter(([,prio])=>prio>1).map(([url,prio])=>'<link rel="stylesheet" href="'+url+'" crossorigin>').join('\n');
+
+
+
+
+function latest(url, {notify}={}) {
+    url = new URL(url).toString();
+    url = url.replace(/u1ui\/([^\/]+)\//, function(x,repoVers){
+        let [repo, vers] = repoVers.split('@');
+        let newVers = repos[repo].release_latest.tag_name.replace('v','');
+        if (notify && vers !== newVers) console.log('new version for: '+url+' :'+newVers)
+        return repo + '@' + newVers + '/';
+    });
+    return url;
+}
+
+
+let repos = null;
+async function loadReposJson(){
+    if (repos) return repos;
+    repos = {};
+    const data = await fetch(import.meta.url + '/../repos.json').then(res=>res.json());
+    const entries = data.forEach(repo=>{
+        repos[repo.name] = repo;
+    });
+}
+
+window.u1 = Object.create(null);
+
+window.u1.hints = async function(){
+    await loadReposJson();
+    let strCss = Object.entries(needed.css).filter(([,prio])=>prio===1).map(([url,prio])=>'<link rel="stylesheet" href="'+latest(url)+'" crossorigin>').join('\n');
+    let strJs  = Object.entries(needed.js).map(([url,prio])=>'<script src="'+latest(url)+'" type=module crossorigin></script>').join('\n');
+    let strCssNonCritical = Object.entries(needed.css).filter(([,prio])=>prio>1).map(([url,prio])=>'<link rel="stylesheet" href="'+latest(url)+'" crossorigin>').join('\n');
     console.log(
         strCss +'\n'+
         strJs + '\n' +
         '\n<!-- non critical at the end -->\n' + strCssNonCritical);
+}
+
+
+window.u1.versionCheck = async function(){
+    await loadReposJson();
+    document.querySelectorAll('script').forEach(el=>{
+        const url = el.src;
+        if (!url.match('u1ui')) return;
+        latest(url, {notify:true});
+    })
+    document.querySelectorAll('link[rel=stylesheet]').forEach(el=>{
+        const url = el.href;
+        if (!url.match('u1ui')) return;
+        latest(url, {notify:true});
+    })
+
 }

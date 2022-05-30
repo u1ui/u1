76d6987bea05271491ee7a4307eb7bdbadb54eca
Tobias Buschor <tobias.buschor@shwups.ch>
2022-05-31 00:28:18 +0200

    add comments, improve

--- auto.js
+++ auto.js
@@ -1,14 +1,31 @@
-console.log('u1ui root?: ', new URL(import.meta.url + '/../../').toString())
+const myUrl = new URL(import.meta.url);
+const debug = myUrl.searchParams.get('debug');
 
 
-let rootUrl = 'https://cdn.jsdelivr.net/gh/u1ui/';
+let root = new URL(myUrl.origin + myUrl.pathname + '/../../');
+//let root = new URL('https://cdn.jsdelivr.net/gh/u1ui/');
+let rootUrl = root.toString();
+
 let min = '.min';
 let useLatest = true;
-// local testing:
-//rootUrl =  import.meta.url + '/../../'; min = ''; useLatest = false; console.log('%cuncomment localhost!','color:red;font-size:1.2em');
+if (root.host === 'localhost') {
+    min = '';
+    useLatest = false;
+}
 
-//import 'https://cdn.jsdelivr.net/gh/nuxodin/lazyfill/mod.js';
-//import 'https://cdn.jsdelivr.net/gh/nuxodin/lazyfill/htmlfills.js';
+// get faster cdn updates? https://purge.jsdelivr.net/gh/nuxodin/cleanup.js@latest/
+
+// local testing:
+// rootUrl =  import.meta.url + '/../../'; min = ''; useLatest = false; console.log('%cuncomment localhost!','color:red;font-size:1.2em');
+
+
+if (debug) { // top level await safari >= 15.1
+    await Promise.all([
+        import('https://cdn.jsdelivr.net/gh/nuxodin/lazyfill@x/mod.min.js'),
+        import('https://cdn.jsdelivr.net/gh/nuxodin/lazyfill@x/htmlfills.min.js'),
+        import('https://cdn.jsdelivr.net/gh/nuxodin/cleanup.js@x/mod.min.js'),
+    ]);
+}
 
 import {importCss} from './utils.js';
 import {latestUrlCached, repos} from './u1.js';
@@ -37,6 +54,12 @@ function impCss(url, options={}){
 window.u1 = Object.create(null);
 window.u1.needed = needed;
 
+////////////////////////////////////////////////////////////////
+//
+//  Import defaults
+//
+////////////////////////////////////////////////////////////////
+
 impCss(rootUrl+'norm.css/norm'+min+'.css');
 impCss(rootUrl+'norm.css/beta'+min+'.css');
 impCss(rootUrl+'base.css/base'+min+'.css');
@@ -53,35 +76,35 @@ impCss(rootUrl+'classless.css/more'+min+'.css');
 
 impJs(rootUrl+'js/init'+min+'.js');
 
+////////////////////////////////////////////////////////////////
+//
+//  Autoimport
+//
+////////////////////////////////////////////////////////////////
+
 
 function newNode(node){
     if (node.tagName.startsWith('U1-')) {
         let name = node.tagName.substring(3).toLowerCase();
-
         if (customElements.get('u1-'+name)) return; // skip if registred
         const base = rootUrl + name + '.el/' + name + min;
         impCss(base + '.css'/*, {for:node}*/);
         impJs(base + '.js');
-
     }
     let classList = node.classList;
     for (let i=0, l=classList.length; i<l; i++) {
         let klass = classList[i]
         if (!klass.startsWith('u1-')) continue;
         let name = klass.substring(3);
-
         impCss(rootUrl + name + '.class/' + name + min + '.css');
-
     }
     const attris = node.attributes;
     for (let i=0, l=attris.length; i<l; i++) {
         let attr = attris[i]
         if (!attr.name.startsWith('u1-')) continue;
-        let name = attr.name.substring(3);
-
+        let name = attr.name.substring(3).replace(/-.*/,''); // example: u1-href-target => href
         // todo: how to check if already added manually?
         impJs(rootUrl + name + '.attr/' + name + min + '.js');
-
     }
 }
 function newNodeRoot(node){
@@ -89,7 +112,6 @@ function newNodeRoot(node){
     newNode(node);
     node.querySelectorAll('*').forEach(newNode);
 }
-
 var mo = new MutationObserver((entries)=>{
     for (let i=0, l=entries.length; i<l; i++) {
         let nodes = entries[i].addedNodes;
@@ -98,8 +120,6 @@ var mo = new MutationObserver((entries)=>{
         }
     }
 });
-//mo.observe(document,{childList:true, subtree:true, characterData:false});
-//newNodeRoot(document.documentElement);
 
 export function addShadowRoot(rootNode){
     mo.observe(rootNode,{childList:true, subtree:true, characterData:false});
@@ -107,12 +127,23 @@ export function addShadowRoot(rootNode){
 }
 addShadowRoot(document.documentElement)
 
+////////////////////////////////////////////////////////////////
+//
+//  UI
+//
+////////////////////////////////////////////////////////////////
+
 addEventListener('keydown',e=>{
     if (e.ctrlKey && e.key ==='F12') {
         import('./auto.ui.js').then(ui=>ui.open())
     }
 });
 
+////////////////////////////////////////////////////////////////
+//
+//  save used files
+//
+////////////////////////////////////////////////////////////////
 
 /* save all */
 function mergeNewlyNeeded(){
@@ -127,5 +158,11 @@ function mergeNewlyNeeded(){
 document.addEventListener('DOMContentLoaded',mergeNewlyNeeded);
 addEventListener('pagehide',mergeNewlyNeeded);
 
+
+////////////////////////////////////////////////////////////////
+//
+//  ready
+//
+////////////////////////////////////////////////////////////////
 
 console.log('%c%s','color:#2c8898;xfont-size:1.3em', '💡 press ctrl+F12 to configure the U1-design-system!');

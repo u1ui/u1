03a9c43fcf24f5265a6078cdc025c132e58d4112
Tobias Buschor <tobias.buschor@shwups.ch>
2023-03-19 03:01:09 +0100

    Update eventsExplorer.js

--- tools/eventsExplorer.js
+++ tools/eventsExplorer.js
@@ -11,7 +11,7 @@ function customRender(obj){
     }
 }
 
-//import {alert} from 'https://cdn.jsdelivr.net/gh/u1ui/dialog.js@3.6.3/dialog.min.js';
+//import {alert} from 'https://cdn.jsdelivr.net/gh/u1ui/dialog.js@3.6.4/dialog.min.js';
 import {alert} from '../../dialog.js@3.6.4/dialog.js';
 
 export class EventsExplorer {
